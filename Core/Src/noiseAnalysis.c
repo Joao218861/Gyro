6f7323d76b93a492627ac04f08cc33f17a56d42d
João V. Santos <joaovictor15015@gmail.com>
2024-08-02 14:29:15 -0300

    SNR calculation

--- Core/Src/noiseAnalysis.c
+++ Core/Src/noiseAnalysis.c
@@ -15,6 +15,7 @@ float f_sum_of_samples_square_value = 0.0;
 
 float f_variance = 0.0;
 float f_standard_deviation = 0.0;
+float f_SNR = 0.0;
 
 // Public Functions
 
@@ -23,6 +24,20 @@ void v_noise_analysis_add_sample_to_pool(float newSample)
 	u32_number_of_samples += 1;
 	f_sum_of_samples_value += newSample;
 	f_sum_of_samples_square_value += pow(newSample,2);
+}
+
+//-- Gets
+
+float f_noise_analysis_get_variance()
+{
+	update_variance();
+	return f_variance;
+}
+
+float f_noise_analysis_get_standard_deviation()
+{
+	update_standard_deviation();
+	return f_standard_deviation;
 }
 
 // Private Functions
@@ -36,4 +51,10 @@ void update_standard_deviation()
 {
 	update_variance();
 	f_standard_deviation = sqrtf(f_variance);
+}
+
+void update_SNR()
+{
+	update_standard_deviation();
+	f_SNR = (f_sum_of_samples_value/u32_number_of_samples)/f_standard_deviation;
 }
